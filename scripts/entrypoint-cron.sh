96a5ae22c9a1ac86c5801a01b5065651026fcd69
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-31 09:08:39 +0100

    Update entrypoint-cron.sh

--- scripts/entrypoint-cron.sh
+++ scripts/entrypoint-cron.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# Point d'entrée qui lance Forgejo ET cron
-
 set -e
 
-# Démarrer cron en arrière-plan (avec l'utilisateur forgejo)
-crond -b -c /etc/crontabs -l 8
+# Démarrer cron en arrière-plan (root)
+crond -b -c /etc/crontabs
 
 # Attendre un peu pour que cron soit bien lancé
 sleep 2
 
-# Démarrer Forgejo (commande originale)
-exec su-exec forgejo:forgejo /usr/local/bin/forgejo web
+# Démarrer Forgejo avec l'utilisateur de l'image officielle
+# L'image Forgejo utilise déjà l'utilisateur approprié
+exec su-exec $(id -u):$(id -g) /usr/local/bin/forgejo web
